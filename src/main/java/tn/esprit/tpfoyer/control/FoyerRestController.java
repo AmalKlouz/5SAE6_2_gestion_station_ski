ce23b5cdf0d7d14f79e24c4c07e59d6ead945a6a
Amal Klouz <80120112+AmalKlouz@users.noreply.github.com>
2024-11-08 14:28:41 +0100

    Update FoyerRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
@@ -1,83 +1,50 @@
 package tn.esprit.tpfoyer.control;
 
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.tpfoyer.entity.Foyer;
 import tn.esprit.tpfoyer.service.IFoyerService;
+
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/foyer")
 public class FoyerRestController {
-    
-    private final IFoyerService foyerService;
-    private final Counter getAllFoyersCounter;
-    private final Counter getFoyerByIdCounter;
-    private final Counter addFoyerCounter;
-    private final Counter deleteFoyerCounter;
-    private final Counter updateFoyerCounter;
-    private final Timer getFoyersTimer;
 
-    public FoyerRestController(IFoyerService foyerService, MeterRegistry registry) {
-        this.foyerService = foyerService;
-        
-        // Initialize counters
-        this.getAllFoyersCounter = Counter.builder("foyer_getall_requests_total")
-            .description("Number of requests to get all foyers")
-            .register(registry);
-            
-        this.getFoyerByIdCounter = Counter.builder("foyer_getbyid_requests_total")
-            .description("Number of requests to get foyer by ID")
-            .register(registry);
-            
-        this.addFoyerCounter = Counter.builder("foyer_add_requests_total")
-            .description("Number of requests to add foyer")
-            .register(registry);
-            
-        this.deleteFoyerCounter = Counter.builder("foyer_delete_requests_total")
-            .description("Number of requests to delete foyer")
-            .register(registry);
-            
-        this.updateFoyerCounter = Counter.builder("foyer_update_requests_total")
-            .description("Number of requests to update foyer")
-            .register(registry);
-            
-        // Initialize timer
-        this.getFoyersTimer = Timer.builder("foyer_getall_duration_seconds")
-            .description("Time taken to retrieve all foyers")
-            .register(registry);
-    }
+    IFoyerService foyerService;
 
+    // http://localhost:8089/tpfoyer/foyer/retrieve-all-foyers
     @GetMapping("/retrieve-all-foyers")
     public List<Foyer> getFoyers() {
-        getAllFoyersCounter.increment();
-        return getFoyersTimer.record(() -> foyerService.retrieveAllFoyers());
+        List<Foyer> listFoyers = foyerService.retrieveAllFoyers();
+        return listFoyers;
     }
-
+    // http://localhost:8089/tpfoyer/foyer/retrieve-foyer/8
     @GetMapping("/retrieve-foyer/{foyer-id}")
     public Foyer retrieveFoyer(@PathVariable("foyer-id") Long fId) {
-        getFoyerByIdCounter.increment();
-        return foyerService.retrieveFoyer(fId);
+        Foyer foyer = foyerService.retrieveFoyer(fId);
+        return foyer;
     }
 
+    // http://localhost:8089/tpfoyer/foyer/add-foyer
     @PostMapping("/add-foyer")
     public Foyer addFoyer(@RequestBody Foyer f) {
-        addFoyerCounter.increment();
-        return foyerService.addFoyer(f);
+        Foyer foyer = foyerService.addFoyer(f);
+        return foyer;
     }
 
+    // http://localhost:8089/tpfoyer/foyer/remove-foyer/{foyer-id}
     @DeleteMapping("/remove-foyer/{foyer-id}")
     public void removeFoyer(@PathVariable("foyer-id") Long fId) {
-        deleteFoyerCounter.increment();
         foyerService.removeFoyer(fId);
     }
 
+    // http://localhost:8089/tpfoyer/foyer/modify-foyer
     @PutMapping("/modify-foyer")
     public Foyer modifyFoyer(@RequestBody Foyer f) {
-        updateFoyerCounter.increment();
-        return foyerService.modifyFoyer(f);
+        Foyer foyer = foyerService.modifyFoyer(f);
+        return foyer;
     }
+
 }
